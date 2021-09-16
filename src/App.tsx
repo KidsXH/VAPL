5e705b171b074c512e1f1e9eb410c06d458fc658
Wen Zhen <wlzeign@hotmail.com>
2021-09-17 05:05:56 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,21 +10,36 @@ import AppHeader from './components/appHeader/AppHeader';
 import { ExecState } from 'unicoen.ts/dist/interpreter/Engine/ExecState';
 import { remove, slot } from './components/emitter';
 
+interface S {
+  execState: ExecState | undefined;
+  lastState: ExecState | undefined;
+}
+
 function App() {
-  const [execState, setExecState] = useState<ExecState | undefined>();
-  const [lastState, setLastState] = useState<ExecState | undefined>();
+  // const [execState, setExecState] = useState<ExecState | undefined>();
+  // const [lastState, setLastState] = useState<ExecState | undefined>();
+
+  const [states, setStates] = useState<S>({
+    execState: undefined,
+    lastState: undefined,
+  });
 
   useEffect(() => {
     // console.log('execState: ' + execState);
-    slot('draw', (execState: ExecState | undefined, lastState: ExecState | undefined) => {
-      setExecState(execState);
-      setLastState(lastState);
-    });
-
+    slot(
+      'draw',
+      (execState: ExecState | undefined, lastState: ExecState | undefined) => {
+        // alert('draw');
+        // console.log('DEBUG| draw');
+        // setExecState(execState);
+        // setLastState(lastState);
+        setStates({ execState, lastState });
+      }
+    );
     return () => {
       remove('draw');
-    }
-  });
+    };
+  }, []);
 
   return (
     <div className="App">
@@ -44,15 +59,15 @@ function App() {
                 <CallStackPanel
                   width={width}
                   height={height}
-                  execState={execState}
-                  lastState={lastState}
+                  execState={states.execState}
+                  lastState={states.lastState}
                 />
               </React.Fragment>
             )}
           </ContainerDimensions>
         </div>
         <div className="Col-3">
-          <MemoryPanel execState={execState} />
+          <MemoryPanel execState={states.execState} />
         </div>
       </div>
     </div>
