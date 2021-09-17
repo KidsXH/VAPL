c3c9d0d3b85c0a26f01ab8f4626f16f0cc8b2f3b
Wen Zhen <wlzeign@hotmail.com>
2021-09-17 23:51:03 +0800

    Update en.ts

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -68,6 +68,9 @@ However, the highlight of the code may be out of place.`,
 // }`
 sourceCodeCcpp: String.raw`#include<stdio.h>
 
+/*print Fahrenheit-Celsius table
+    for fahr = 0, 20, ..., 300 */
+
 int main() {
   int fahr, celsius;
   int lower, upper, step;
